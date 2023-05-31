0c3a9b737efe3b97b47e83b7e94fe968bd7b43f5
jdabassett <jacobbassett@gmail.com>
2023-05-31 16:48:26 -0600

    small css changes to About and Footer

--- src/About/About.js
+++ src/About/About.js
@@ -2,34 +2,52 @@ import { Component } from "react";
 import Carousel from "react-bootstrap/Carousel";
 import rhettImage from '../images/rhett-beardemphl.jpg';
 import jacobImage from '../images/jacob-bassett.jpg'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class Profile extends Component {
   render() {
     return (
-      <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            style={{ height: "500px" }}
-            src={rhettImage}
-            alt={"rhett's profile pic"}
-          />
-          <Carousel.Caption>
-            <h3>Rhett Beardemphl</h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            style={{ height: "500px" }}
-            src={jacobImage}
-            alt={"jacob's profile pic"}
-          />
-          <Carousel.Caption>
-            <h3>Jacob Bassett</h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+    <Container className="carouselContainer">
+      <Row className="justify-content-md-center">
+        <Col 
+          xs={10} md={10} lg={10}
+          style={{ width:"500px" }}>
+          <Carousel
+            className="carouselParent"
+            interval={null}
+            >
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                style={{ height: "500px", width:"500px" }}
+                src={rhettImage}
+                alt={"rhett's profile pic"}
+              />
+              <Carousel.Caption
+                className="carouselCaption">
+                <h3 className="textAboutUs">Rhett Beardemphl</h3>
+                <p className="textAboutUs"></p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                style={{ height: "500px"}}
+                src={jacobImage}
+                alt={"jacob's profile pic"}
+              />
+              <Carousel.Caption
+                className="carouselCaption">
+                <h3 className="textAboutUs">Jacob Bassett</h3>
+                <p className="textAboutUs">Jacob was a molecular biologist turned code slinger who like the taste of nucleotides with his glutamate.<a className="link" href="https://www.youtube.com/watch?v=GHX3aaLmjMI"> [link]</a></p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </Col>
+      </Row>
+    </Container>
     );
   }
 }
--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import './Footer.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class Footer extends React.Component {
   render() {
     return (
-      <Navbar collapseOnSelect expand="lg" className='footer'>
-        <Navbar.Brand className="footerText">&copy;Rhett&Jacob</Navbar.Brand>
-      </Navbar>
+      <Container className="carouselContainer">
+        <Row className="justify-content-md-center">
+          <Col xs={10} md={10} lg={10}>
+            <Navbar collapseOnSelect expand="lg" className='footer'>
+              <Navbar.Brand className="footerText">&copy;Rhett&Jacob</Navbar.Brand>
+            </Navbar>
+          </Col>
+        </Row>
+      </Container>
     )
   }
 }
