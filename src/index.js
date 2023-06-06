74218c3d776866d56dc97f72c1397fc19edf0b6a
jdabassett <jacobbassett@gmail.com>
2023-06-06 13:17:15 -0600

    login and logout buttons added but not working

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App/App';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* 6-6-23 */}
+    <Auth0Provider
+          domain={process.env.REACT_APP_AUTH_DOMAIN}
+          clientId={process.env.REACT_APP_AUTH_CLIENTID}
+          authorizationParams={{
+            redirect_uri: process.env.REACT_APP_AUTH_URI
+          }}>
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
